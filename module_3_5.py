d54b5c90d4fb097643f7bc0a4a8d78cb5a08574c
olga <helga_black@list>
2024-12-20 10:19:22 +0300

    final_module_3_5

--- module_3_5.py
+++ module_3_5.py
@@ -1,11 +1,11 @@
 def get_multiplied_digits(number):
-    str_number=str(number)
-    first=int(str_number[0])
-    if number ==0:
-        return 0
+    str_number=str(number)     # Преобразуем число в строку для удобной работы с цифрами
+    first=int(str_number[0])   # Берем первую цифру числа
+    if len(str_number) ==2 and int(str_number[1]) %10 == 0:   #Базовый случай: если длина строки равна 2 и без остатка делится на 10, возвращаем первую цифру
+        return first
     else:
         if len(str_number)>1:
-            result = first * get_multiplied_digits(int(str_number[1:]))
+            result = first * get_multiplied_digits(int(str_number[1:]))    # Возвращаем произведение первой цифры и результата рекурсии
             return result
         if len(str_number) <= 1:
             return first
@@ -13,27 +13,3 @@ result = get_multiplied_digits(40203)
 print(result)
 result2 = get_multiplied_digits(402030)
 print(result2)
-
-# вывод:
-# 24
-# 0
-
-# def get_multiplied_digits(number):
-#     str_number=str(number)
-#     first=int(str_number[0])
-#     if number ==0:
-#         return 1
-#     else:
-#         if len(str_number)>1:
-#             result = first * get_multiplied_digits(int(str_number[1:]))
-#             return result
-#         if len(str_number) <= 1:
-#             return first
-# result = get_multiplied_digits(40203)
-# print(result)
-# result2 = get_multiplied_digits(402030)
-# print(result2)
-#
-#  вывод:
-#  24
-#  24
